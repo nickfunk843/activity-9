ba1cf9ea27142e2a7819c1825c35a95673d6b9ca
nickfunk843 <93162070+nickfunk843@users.noreply.github.com>
2021-12-02 19:17:16 -0800

    Add files via upload

---
+++ primes.py
@@ -0,0 +1,16 @@
+def is_prime(number):
+    if number <=1:
+        return False
+
+    for element in range(2,number):
+        if number % element == 0:
+            return False
+
+    return True
+
+def print_next_prime(number):
+    index = number
+    while True:
+        index += 1
+        if is_prime(index):
+            print(index)
